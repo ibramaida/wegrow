d2651acd908bd1a46800c58d9da9361d3389b192
ibramaida <bmpcbob@gmail.com>
2024-02-26 13:08:35 +0100

    our process done

--- scroller.js
+++ scroller.js
@@ -7,5 +7,14 @@ if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 function addAnimation() {
   scrollers.forEach((scroller) => {
     scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".inner");
+    const innerContents = Array.from(scrollerInner.children);
+
+    innerContents.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
   });
 }
